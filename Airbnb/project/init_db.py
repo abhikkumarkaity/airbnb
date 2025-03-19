7a8edc650895ffd9db9c7ebe3b549b4f57eb56b1
Abhik Kumar Kaity <142738054+abhikkumarkaity@users.noreply.github.com>
2025-03-20 01:18:31 +0530

    Delete Airbnb/project

--- Airbnb/project/init_db.py
+++
@@ -1,6 +0,0 @@
-from db.session import Base, engine
-from db.models import User, OTP
-
-print("Creating tables...")
-Base.metadata.create_all(bind=engine)
-print("Tables created ✅")
--- Airbnb/project/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from routes import auth
-
-app = FastAPI()
-app.include_router(auth.router)
